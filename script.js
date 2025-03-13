361b02d0d9a6eae42f9f65a64dc37684c1e9cceb
Evie <Alexbetson123@gmail.com>
2025-03-13 19:40:08 +0000

    dave

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log('Hit me up on discord :3\n@Atomixal')
+window.addEventListener('DOMContentLoaded', () => {
+    VANTA.BIRDS({
+        el: "#vanta",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        backgroundColor: 0x70e2f,
+        color1: 0xd200ff,
+        color2: 0x3d892a,
+        wingSpan: 17.00,
+        separation: 100.00,
+        alignment: 1.00,
+        cohesion: 14.00
+      })    
+});
